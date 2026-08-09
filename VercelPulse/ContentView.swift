e6e159a422494dd039b2313a5419013675be9105
mnashed13 <44353375+mnashed13@users.noreply.github.com>
2026-08-09 14:55:47 +1000

    Initial Commit

---
+++ VercelPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VercelPulse
+//
+//  Created by Michael Nashed  on 9/8/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VercelPulse/VercelPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VercelPulseApp.swift
+//  VercelPulse
+//
+//  Created by Michael Nashed  on 9/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct VercelPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
